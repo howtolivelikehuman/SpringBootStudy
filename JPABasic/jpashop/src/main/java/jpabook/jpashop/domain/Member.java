3024e0092c5cc57080361988891ee8cc3c27b4e0
howtolivelikehuman <yoich_@naver.com>
2021-05-01 19:21:33 +0900

    value type

--- JPABasic/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPABasic/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -12,13 +12,21 @@ public class Member extends BaseEntity {
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     //사실 실제로는 쓸 필요가 없음
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
     public Long getId() {
         return id;
@@ -34,30 +42,6 @@ public class Member extends BaseEntity {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
     }
 }
 
