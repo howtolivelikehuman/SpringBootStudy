77d8eb89d098626545c3eea9a0dbe21d100f833c
howtolivelikehuman <yoich_@naver.com>
2021-07-31 16:49:14 +0900

    no message

--- SpringDataJPA/data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ SpringDataJPA/data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,12 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -25,8 +28,15 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable){
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 20; i++) {
+            memberRepository.save(new Member("user"+i, i));
+        }
     }
 }
